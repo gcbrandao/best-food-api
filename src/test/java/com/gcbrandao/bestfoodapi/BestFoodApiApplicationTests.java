42ea7107114840be0d9320d1966b95f9b5434177
Gil Brandao <gcbrandao@gmail.com>
2020-01-09 15:17:17 -0300

    primeiro commit de inincio de projeto

---
+++ src/test/java/com/gcbrandao/bestfoodapi/BestFoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcbrandao.bestfoodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestFoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
