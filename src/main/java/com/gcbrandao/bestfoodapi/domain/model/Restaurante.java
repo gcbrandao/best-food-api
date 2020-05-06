ab7f71abedf4e86119fef7939ce0a7c8fef9b1f2
Gilberto Calegari Brandão <gilbertobrandao.sinqia@sulamerica.com.br>
2020-05-05 22:48:10 -0300

    incluindo dataCriacao e dataAtualizacao

--- src/main/java/com/gcbrandao/bestfoodapi/domain/model/Restaurante.java
+++ src/main/java/com/gcbrandao/bestfoodapi/domain/model/Restaurante.java
@@ -1,6 +1,7 @@
 package com.gcbrandao.bestfoodapi.domain.model;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,6 +20,8 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -43,6 +46,16 @@ public class Restaurante {
     @JsonIgnore
     @Embedded
     private Endereco endereco;
+
+    @JsonIgnore
+    @CreationTimestamp
+    @Column(nullable = false, columnDefinition = "datetime")
+    private LocalDateTime dataCadastro;
+
+    @JsonIgnore
+    @UpdateTimestamp
+    @Column(nullable = false, columnDefinition = "datetime")
+    private LocalDateTime dataAtualizacao;
 
     @JsonIgnore
     @ManyToMany
